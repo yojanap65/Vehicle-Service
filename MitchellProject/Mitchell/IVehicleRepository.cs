a0698844b5acd4ac4ea385872bfcbcaed4c404ec
Patil <ypatil@illumina.com>
2016-08-18 23:13:40 -0700

    project uploaded

---
+++ MitchellProject/Mitchell/IVehicleRepository.cs
@@ -0,0 +1,18 @@
+﻿using Mitchell.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mitchell
+{
+    interface IVehicleRepository
+    {
+        IEnumerable<Vehicle> GetVehicle();
+        Vehicle GetById(String id);
+        void Update(Vehicle vehicle);
+        void DeleteById(String id);
+
+    }
+}
